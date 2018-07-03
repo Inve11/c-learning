d76d6c06f310ee90d438fac1776f47510d8b89f4
Inve11 <Carmello2006@gmail.com>
2018-07-03 12:35:38 +0300

    add c# learning

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.SmallBasic.Library;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trtltraning
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Turtle.Speed = 10;
+            int i = 0;
+            while (i < 8) {
+                Turtle.Move(25);
+                Turtle.TurnRight();
+                Turtle.Move(25);
+                Turtle.TurnRight();
+                Turtle.Move(25);
+                Turtle.TurnLeft();
+                Turtle.Move(25);
+                Turtle.TurnLeft();
+                i++;
+            }
+        }
+    }
+}
